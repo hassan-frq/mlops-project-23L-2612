966679d2f0a4febd54790cfa2ccd8d9c9e6b6f14
hassan-frq <umer.sngpl@gmail.com>
2026-09-05 14:01:36 +0500

    Add data normalization step - 23L-2612

--- src/train_23L-2612.py
+++ src/train_23L-2612.py
@@ -1,10 +1,8 @@
-# train_23L-2612.py
-# Student ID: 23L-2612
-
 import pandas as pd
 import joblib
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 STUDENT_ID = "23L-2612"
 
@@ -14,6 +12,8 @@ def main():
 
     X = df.drop(columns=["MedHouseVal"])
     y = df["MedHouseVal"]
+
+    X = StandardScaler().fit_transform(X)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
